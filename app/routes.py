5ccfe278ad9ebc848fd5c2ef00c42af7d488da50
david <davidpierrealves21@gmail.com>
2019-02-01 01:08:32 -0300

    Added first templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,29 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'David'}
+
+    posts = [
+        {
+            'author': {'username': 'Michelangelo'},
+            'body': 'Beautiful day in Recife!'
+        },
+        {
+            'author': {'username': 'Rafael'},
+            'body': 'Rainy day in Recife!'
+        },
+        {
+            'author': {'username': 'Leonardo'},
+            'body': 'Hot day in Recife!'
+        },
+        {
+            'author': {'username': 'Donatello'},
+            'body': 'Its day in Recife!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
