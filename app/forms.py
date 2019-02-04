9d407a3336357d883bd56aab481f3f1464428e49
david <davidpierrealves21@gmail.com>
2019-02-04 11:10:52 -0300

    Added profile pages and edit profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from flask_wtf import FlaskForm
 from app.models import User
 
 
@@ -28,3 +28,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please, use a different email address')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = StringField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Save changes')
